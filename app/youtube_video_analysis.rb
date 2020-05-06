d855bf574f8fcc1cb7510c294ca7d1d008070205
yuya sato <ysato.5654@gmail.com>
2020-05-06 21:46:01 +0800

    update main file - add execution option - error handle

--- app/youtube_video_analysis.rb
+++ app/youtube_video_analysis.rb
@@ -12,22 +12,42 @@ def youtube_video_analysis
 
 	params = Hash.new
 
+	opt.on('-g', '--get', 'get comments from desired video') { |v| params[:get] = v }
+	opt.on('-a', '--analyze', 'analyze desired video comments') { |v| params[:analyze] = v }
 	opt.on('-e [VAL]', '--environment [VAL]', 'environment (default: production)') { |v| params[:environment] = v }
 	opt.on('-v [VAL]', '--video_id [VAL]', 'video id') { |v| params[:video_id] = v }
 
-	opt.parse(ARGV)
+	begin
+		opt.parse(ARGV)
+	rescue SystemExit => e
+		return
+	rescue Exception => e
+		#e.class
+		# => OptionParser::InvalidOption
+
+		STDERR.puts "#{__FILE__}:#{__LINE__}:Error: #{e}"
+		return
+	end
 
 	environment = params.key?(:environment) ? params[:environment] : DEFAULT_ENVIRONMENT
 	video_id = params.key?(:video_id) ? params[:video_id] : ''
 
 	environment = '' unless Ytvarb::ENVIRONMENT.include?(environment)
 
+	# check option - environment
 	if environment.nil? or environment.empty?
 		STDERR.puts "#{__FILE__}:#{__LINE__}:Error: option error. usage is 'ruby #{File.basename(__FILE__)} --help'"
 		return
 	end
 
+	# check option - video id
 	if video_id.nil? or video_id.empty?
+		STDERR.puts "#{__FILE__}:#{__LINE__}:Error: option error. usage is 'ruby #{File.basename(__FILE__)} --help'"
+		return
+	end
+
+	# check option - execution type
+	if params[:get].nil? and params[:analyze].nil?
 		STDERR.puts "#{__FILE__}:#{__LINE__}:Error: option error. usage is 'ruby #{File.basename(__FILE__)} --help'"
 		return
 	end
@@ -43,18 +63,25 @@ def youtube_video_analysis
 	youtube_comment = Ytvarb::YoutubeComment.new
 
 	# get comment from youtube video
-	begin
-		unless youtube_comment.get
+	if params[:get]
+		STDOUT.puts "get youtube comment"
+		STDOUT.puts
+
+		begin
+			unless youtube_comment.get
+				return
+			end
+		rescue Exception => e
+			STDERR.puts "#{__FILE__}:#{__LINE__}:Error: #{e}: need to contact the ytvarb developers"
 			return
 		end
-	rescue Exception => e
-		STDERR.puts "#{__FILE__}:#{__LINE__}:Error: #{e}: need to contact the ytvarb developers"
-		return
 	end
 
 	# analyze youtube comment
-	unless youtube_comment.analyze
-		return
+	if params[:analyze]
+		unless youtube_comment.analyze
+			return
+		end
 	end
 
 end
